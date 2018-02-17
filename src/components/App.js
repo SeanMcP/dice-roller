5b054b5475d7f063024aff1b80992f670741889a
Sean McPherson <seanmcp25@gmail.com>
2018-02-17 00:31:34 -0500

    Set up project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../styles/app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app-container">
+      </div>
+    );
+  }
+}
+
+export default App;
