8ee382b27fc513db698c5e7e9fcdedde9a770e5c
Sean McPherson <seanmcp25@gmail.com>
2018-02-26 23:37:27 -0500

    Add roll.wav file

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      soundOn: false,
+      soundOn: true,
       dice: ['initial&20&inverted'],
       menuOpen: false,
     };
