a087124495bdc5568904ba0a281556c32250d838
Sean McPherson <mail@seanmcp.com>
2019-05-29 22:07:24 -0400

    Use form, submit to create custom die
    Fixes #1

--- src/components/CreateMenu.js
+++ src/components/CreateMenu.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Emoji from 'a11y-react-emoji';
-import IconButton from './functional/IconButton';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Emoji from "a11y-react-emoji";
+import IconButton from "./functional/IconButton";
 
 class CreateMenu extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      newDie: '',
-      validateInput: ''
+      newDie: "",
+      validateInput: ""
     };
 
     this._handleChange = this._handleChange.bind(this);
@@ -18,24 +18,24 @@ class CreateMenu extends Component {
   render() {
     const diceInfo = {
       dnd: [
-        { sides: 4, style: 'green' },
-        { sides: 6, style: 'green' },
-        { sides: 8, style: 'green' },
-        { sides: 10, style: 'green' },
-        { sides: 12, style: 'green' },
-        { sides: 20, style: 'green' },
-        { sides: 100, style: 'green' },
+        { sides: 4, style: "green" },
+        { sides: 6, style: "green" },
+        { sides: 8, style: "green" },
+        { sides: 10, style: "green" },
+        { sides: 12, style: "green" },
+        { sides: 20, style: "green" },
+        { sides: 100, style: "green" }
       ],
       settlers: [
-        { sides: 6, style: 'settlers-red' },
-        { sides: 6, style: 'settlers-yellow' },
+        { sides: 6, style: "settlers-red" },
+        { sides: 6, style: "settlers-yellow" }
       ],
       yahtzee: [
         { sides: 6 },
         { sides: 6 },
         { sides: 6 },
         { sides: 6 },
-        { sides: 6 },
+        { sides: 6 }
       ]
     };
     const addOne = this.props.addOne;
@@ -45,44 +45,41 @@ class CreateMenu extends Component {
       <ul className="create-menu modal-menu create">
         <li className="heading">Create</li>
         <li>
-          <div className={"custom-die" + (isError ? ' error' : '')}>
-              <input
-                type="number"
-                placeholder="Custom die"
-                value={this.state.newDie}
-                onChange={this._handleChange}
-              />
-              {isError ? (
-                <div className="error-message">
-                  <i className="material-icons md-18">warning</i>
-                  {this.state.validateInput}
-                </div>
-              ) : null}
-              <IconButton
-                className="create"
-                icon="add_circle"
-                label="Create"
-                onClick={this._handleCreate}
-              />
-            </div>
+          <form
+            className={"custom-die" + (isError ? " error" : "")}
+            onSubmit={this._handleSubmit}
+          >
+            <input
+              type="number"
+              placeholder="Custom die"
+              value={this.state.newDie}
+              onChange={this._handleChange}
+            />
+            {isError ? (
+              <div className="error-message">
+                <i className="material-icons md-18">warning</i>
+                {this.state.validateInput}
+              </div>
+            ) : null}
+            <IconButton
+              className="create"
+              icon="add_circle"
+              label="Create"
+              type="submit"
+            />
+          </form>
         </li>
-        <li
-          className="button"  
-          onClick={() => addMany(diceInfo.dnd)}>
-            <Emoji label="swords" symbol="⚔️" contextClass="li"/>
-            D&D set
+        <li className="button" onClick={() => addMany(diceInfo.dnd)}>
+          <Emoji label="swords" symbol="⚔️" className="li" />
+          D&D set
         </li>
-        <li
-          className="button"  
-          onClick={() => addMany(diceInfo.settlers)}>
-            <Emoji label="sheep" symbol="🐑"  contextClass="li"/>
-            Settlers
+        <li className="button" onClick={() => addMany(diceInfo.settlers)}>
+          <Emoji label="sheep" symbol="🐑" className="li" />
+          Settlers
         </li>
-        <li
-          className="button"  
-          onClick={() => addMany(diceInfo.yahtzee)}>
-            <Emoji symbol="🙋"  contextClass="li"/>
-            Yahtzee
+        <li className="button" onClick={() => addMany(diceInfo.yahtzee)}>
+          <Emoji symbol="🙋" className="li" />
+          Yahtzee
         </li>
         <li>
           <span onClick={() => addOne(6)}>D6</span>
@@ -92,32 +89,37 @@ class CreateMenu extends Component {
           <span onClick={() => addOne(100)}>D100</span>
         </li>
       </ul>
-    )
+    );
   }
 
   _handleChange(e) {
     e.preventDefault();
-    this.setState({ newDie: e.target.value, validateInput: '' });
+    this.setState({ newDie: e.target.value, validateInput: "" });
   }
 
   _handleCreate() {
     const num = Number(this.state.newDie);
-    if (typeof num !== 'number') {
-      this.setState({ validateInput: 'Must be a number' })
+    if (typeof num !== "number") {
+      this.setState({ validateInput: "Must be a number" });
     } else if (num < 2) {
-      this.setState({ validateInput: 'Must be greater than 1' })
+      this.setState({ validateInput: "Must be greater than 1" });
     } else if (num > 999999999) {
-      this.setState({ validateInput: 'Must be less than 1x10^9' })
+      this.setState({ validateInput: "Must be less than 1x10^9" });
     } else {
       this.props.addOne({ sides: Number(this.state.newDie) });
-      this.setState({ newDie: '' });
+      this.setState({ newDie: "" });
     }
   }
-};
+
+  _handleSubmit = e => {
+    e.preventDefault();
+    this._handleCreate();
+  };
+}
 
 CreateMenu.propTypes = {
   addMany: PropTypes.func.isRequired,
-  addOne: PropTypes.func.isRequired,
+  addOne: PropTypes.func.isRequired
 };
 
 export default CreateMenu;
